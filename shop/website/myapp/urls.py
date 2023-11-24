71f5c8bd5c46dfcc0b1a24c5598edcfe0a6aba04
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2023-11-24 13:45:35 +0300

    ignore

--- shop/website/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from myapp.views import *
-
-urlpatterns = [
-    path('hello/', index),
-    path('hello/<int:my_id>/', indexItem),
-
-    path('contacts/', contact),
-]
