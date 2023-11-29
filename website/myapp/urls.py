68872e6e8bc988b8eef3dc1cdb751e587d349990
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2023-11-29 23:25:13 +0300

    updating data

--- website/myapp/urls.py
+++ website/myapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', index),
 # name нужно чтобы каждый раз не вводить путь url (index.html)
     path('<int:my_id>/', indexItem, name="detail"),
+    path('additem/', add_item, name="add_item"),
+    path('udpateitem/<int:my_id>/', update_item, name="update_item"),
 ]
