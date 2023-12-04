b3da24ab0d315aba86a0e894a69468f7d74f8b46
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2023-12-04 15:51:48 +0300

    CBN views

--- website/myapp/urls.py
+++ website/myapp/urls.py
@@ -4,9 +4,10 @@ from myapp.views import *
 app_name = "myapp"
 
 urlpatterns = [
-    path('', index, name="index"),
+    # path('', index, name="index"),
+    path('', ProductListView.as_view(), name="index"),
 # name нужно чтобы каждый раз не вводить путь url (index.html)
-    path('<int:my_id>/', indexItem, name="detail"),
+    path('<int:pk>/', ProductDetailView.as_view(), name="detail"),
     path('additem/', add_item, name="add_item"),
     path('udpateitem/<int:my_id>/', update_item, name="update_item"),
     path('delete/<int:my_id>/', delete_item, name="delete_item"),
