e1839ee0e64b99f4247555e84a9e0d325c821bd6
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2023-12-04 11:41:35 +0300

    models users

--- website/users/models.py
+++ website/users/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, upload_to='_profile_image')
+    contact_number = models.CharField(max_length=50, default='0123456789')
+
+    def __str__(self):
+        return self.user.username
